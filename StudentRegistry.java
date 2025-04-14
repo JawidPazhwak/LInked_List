a9359c6906179b97bab7c699656a7b73dd956c10
Jawid <jawid5839@gmail.com>
2025-04-14 19:27:13 +0600

    Create StudentRegistry.java
    Simple Student Registry System.

---
+++ StudentRegistry.java
@@ -0,0 +1,29 @@
+public class StudentRegistery {
+    public Student head;
+
+    public void addStudent(Student s){
+        if(head == null){
+            head = s;
+        }
+        else{
+            Student current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = s;
+            System.out.println(s.name + " added");
+        }
+    }
+    public void showStudents(){
+        if(head == null){
+            System.out.println("No student");
+        }
+        else{
+            Student current = head;
+            while(current != null){
+                current.details();
+                current = current.next;
+            }
+        }
+    }
+}
